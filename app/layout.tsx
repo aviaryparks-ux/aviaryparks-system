bf9849b1b02ba32a6952ff5d94af5b8104c2fcff
handi <aviaryparks@gmail.com>
2026-03-23 17:49:00 +0700

    fix typescript error

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
 import "./globals.css";
-import "leaflet/dist/leaflet.css"; // WAJIB untuk map
+import "leaflet/dist/leaflet.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,13 +33,9 @@ export default function RootLayout({
         h-full
       `}
     >
-
       <body className="min-h-full">
-
         {children}
-
       </body>
-
     </html>
   );
 }
